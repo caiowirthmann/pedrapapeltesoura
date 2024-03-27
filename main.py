d0abce37bc3a8426004af13f2cba6729f99f81dc
Caio Wirthmann <caiowirthmann@gmail.com>
2024-03-27 12:00:39 -0300

    adicionado metodo para jogar infinitamente

--- main.py
+++ main.py
@@ -6,23 +6,29 @@ jogadas = {
     3 : "tesoura"
 }
 
-jogada_cpu = jogadas[(random.randrange(1,4))]
+def gerar_jogada_cpu():
+    jogada_cpu = jogadas[(random.randrange(1,4))]
+    return jogada_cpu
 
 print (" ============== INICIO DE JOGO ============== ")
 print ()
 
-while True:
-    jogada_jogador = input("Digite a sua jogada: (Pedra - Papel - Tesoura) --> ").lower()
-    if jogada_jogador in jogadas.values():
-        break
-    else:
-        print (f'{jogada_jogador} não é uma opção valida') 
+def gerar_jogada_jogador():
+    while True:
+        jogada_jogador = input("Digite a sua jogada: (Pedra - Papel - Tesoura): ").lower()
+        if jogada_jogador in jogadas.values():
+            break
+        else:
+            print (f'{jogada_jogador} não é uma opção valida. Escolha Pedra - Papel - Tesoura') 
+    return jogada_jogador
 
 # define ganhador
 def jogar(cpu, player):
+    # talvez encapsular a função inteira em um while true para rodar quantas vezes o jogador quiser
     print()
-    print(f'O computador jogou {cpu}')
+    print(f'O computador jogou: {cpu}')
     print()
+
     if cpu == player:
         print (" ================ EMPATE! ================ ")
 
@@ -40,8 +46,30 @@ def jogar(cpu, player):
         print (" ================ VITÓRIA ================ ")
     if player == "tesoura" and cpu == "pedra":
         print (" ================ DERROTA ================ ")
-    
     print ()
-    print (" ============== FIM DE JOGO ============== ")
+    #print (" ============== FIM DE JOGO ============== ")
 
-jogar(jogada_cpu, jogada_jogador)       
+def jogar_novamente():
+    while True:
+        jogar_novamente = input("Deseja jogar novamente? Digite S para (sim) ou N para (não) ")
+        if jogar_novamente in ("s", "S", "n", "N"):
+            break
+        else:
+            print ("Digite uma opção valida")
+
+    if jogar_novamente in ("n", "N"):
+        print (" ============== FIM DE JOGO ============== ")
+        return False
+    else:
+        return True
+
+def main():
+    while True:
+        jogada_cpu = gerar_jogada_cpu()
+        jogada_jogador = gerar_jogada_jogador()
+        jogar(jogada_cpu, jogada_jogador)
+        novo_jogo = jogar_novamente()
+        if novo_jogo == False:
+            break
+        
+main() 
