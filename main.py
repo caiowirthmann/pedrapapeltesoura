aa31f92f74612888fe59d415a909b04bc6666fc1
caiowirthmann <caiowirthmann@gmail.com>
2024-03-26 22:03:06 -0300

    concluido mas pode ser melhorado

--- main.py
+++ main.py
@@ -6,20 +6,41 @@ jogadas = {
     3 : "tesoura"
 }
 
-jogada_cpu = random.randrange(1,4)
+jogada_cpu = jogadas[(random.randrange(1,4))]
 
-jogada_jogador = input("Digite a sua jogada: Pedra - Papel - Tesoura --> ").lower()
+print (" ============== INICIO DE JOGO ============== ")
+print ()
 
-# adicionar um while para checar se a jogada é valida 
-if jogada_jogador not in jogadas.values():
-    print (f'{jogada_jogador} não é uma opção valida')
-else:
-    print ('Jogada valida')
+while True:
+    jogada_jogador = input("Digite a sua jogada: (Pedra - Papel - Tesoura) --> ").lower()
+    if jogada_jogador in jogadas.values():
+        break
+    else:
+        print (f'{jogada_jogador} não é uma opção valida') 
 
 # define ganhador
 def jogar(cpu, player):
+    print()
+    print(f'O computador jogou {cpu}')
+    print()
     if cpu == player:
-        print (" ===== EMPATE ===== ")
-    print (" ===== Você ganhou ===== ")
+        print (" ============== EMPATE ============== ")
 
-jogar(jogada_cpu, jogada_jogador)
+    # simples mas pode ser que tenha um jeito melhor de fazer
+    # esse cruzamento para checar a combinação
+    if player == "pedra" and cpu == "tesoura":
+        print (" ============== VITÓRIA ============== ")
+    if player == "pedra" and cpu == "papel":
+        print (" ============== DERROTA ============== ")
+    if player == "papel" and cpu == "tesoura":
+        print (" ============== DERROTA ============== ")
+    if player == "papel" and cpu == "pedra":
+        print (" ============== VITÓRIA ============== ")
+    if player == "tesoura" and cpu == "papel":
+        print (" ============== VITÓRIA ============== ")
+    if player == "tesoura" and cpu == "pedra":
+        print (" ============== DERROTA ============== ")
+    
+print (" ============== FIM DE JOGO ============== ")
+
+jogar(jogada_cpu, jogada_jogador)       
